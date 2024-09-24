36d0bc00da6882a794c2bf9ab1a9135e76dd4afb
bob Hanni <yocorangelo18@gmail.com>
2024-09-24 11:12:49 +0800

    may bug pa to, access blocked : your smile req is invalid pa sa auth ayusin ko nalang mamaya

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native";
 import { Redirect } from "expo-router";
 
 export default function Index() {
+ 
    const user = false
   return (
     <View
--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
-export default function index() {
+WebBrowser.maybeCompleteAuthSession();
+
+export default function Index() {
+  const webClientId = "907374281624-gvg831t96pcbd2snuhno0uqv5e18ptgo.apps.googleusercontent.com";
+  const androidClientId = "907374281624-16dsi7qf6kci9i2j5sivc3tnj9jtlgtc.apps.googleusercontent.com";
+
+  const [error, setError] = useState();
+  const [userInfo, setUserInfo] = useState();
+
+  const config = {
+    webClientId,
+    androidClientId,
+     scopes: ['profile', 'email'], // Add necessary scopes
+  };
+
+  const [request, response, promptAsync] = Google.useAuthRequest(config);
+
+  const handleToken = () => {
+    if (response && response.type === "success") {
+      const { authentication } = response;
+      const token = authentication.accessToken;
+      console.log("access token", token);
+    }
+  };
+
+  useEffect(() => {
+    handleToken();
+  }, [response]);
+
   return (
-    <View>
-      <Text>index</Text>
+    <View style={{paddingTop:200}}>
+      <TouchableOpacity onPress={() => promptAsync()}>
+        <Text>Login here</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
